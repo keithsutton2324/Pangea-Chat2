ea8aa9aff264a02b33b1ef83beb980dbb63f55fb
Your Name <suttonk@bcglobal.net>
2021-03-28 15:58:38 -0700

    removed heroku-postbuild in script

--- server.js
+++ server.js
@@ -14,17 +14,18 @@ const {
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-/*
-// routes
-app.get('/', (req, res) => {
-	res.send('Hello from MERN');
-});
-*/
 
+// routes
+app.get("/", function(req, res) {
+      res.json(path.join(__dirname, "Public/index.html"));
+});
+
+    /*
 app.use(express.static(path.join(__dirname, 'public')));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
+*/
 
 const botName = 'Local Time';
 
